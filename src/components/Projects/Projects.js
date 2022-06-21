f88c1575f135de84ecbf26fb53996e288a2ebb32
Foy5al <mh.foysal.h@gmail.com>
2022-06-21 13:46:06 +0600

    project responsive

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,8 @@ import Particles from 'react-tsparticles';
 import Transition from '../../Transition';
 import './Projects.scss';
 import { loadFull } from "tsparticles";
+import { Link } from 'react-router-dom';
+
 
 const Projects = () => {
     const project = gsap.timeline();
@@ -36,104 +38,139 @@ const Projects = () => {
         }, "-=3")
     }, [])
     return (
-        <div>
+        <>
             <Transition timeline={project} />
 
-            {/* <div className="project-image project-overlay" > */}
-            {/* <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={{
-                    background: {
-                        color: {
-                            value: "#222831",
+            <div id='project'>
+
+
+                {/* <div className="project-image project-overlay" > */}
+                <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={{
+                        background: {
+                            color: {
+                                value: "#222831",
+                            },
                         },
-                    },
-                    fpsLimit: 120,
-                    interactivity: {
-                        events: {
-                            onClick: {
+                        fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onClick: {
+                                    enable: true,
+                                    mode: "push",
+                                },
+                                onHover: {
+                                    enable: true,
+                                    mode: "repulse",
+                                },
+                                resize: true,
+                            },
+                            modes: {
+                                push: {
+                                    quantity: 4,
+                                },
+                                repulse: {
+                                    distance: 200,
+                                    duration: 0.4,
+                                },
+                            },
+                        },
+                        particles: {
+                            color: {
+                                value: "#F2F2F2",
+                            },
+
+                            collisions: {
                                 enable: true,
-                                mode: "push",
                             },
-                            onHover: {
+                            move: {
+                                direction: "none",
                                 enable: true,
-                                mode: "repulse",
+                                outModes: {
+                                    default: "bounce",
+                                },
+                                random: false,
+                                speed: 3,
+                                straight: false,
                             },
-                            resize: true,
-                        },
-                        modes: {
-                            push: {
-                                quantity: 4,
+                            number: {
+                                density: {
+                                    enable: true,
+                                    area: 800,
+                                },
+                                value: 80,
                             },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4,
+                            opacity: {
+                                value: 0.5,
+                            },
+                            shape: {
+                                type: "circle",
+                            },
+                            size: {
+                                value: { min: 1, max: 5 },
                             },
                         },
-                    },
-                    particles: {
-                        color: {
-                            value: "#F2F2F2",
-                        },
-
-                        collisions: {
-                            enable: true,
-                        },
-                        move: {
-                            direction: "none",
-                            enable: true,
-                            outModes: {
-                                default: "bounce",
-                            },
-                            random: false,
-                            speed: 3,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                area: 800,
-                            },
-                            value: 80,
-                        },
-                        opacity: {
-                            value: 0.5,
-                        },
-                        shape: {
-                            type: "circle",
-                        },
-                        size: {
-                            value: { min: 1, max: 5 },
-                        },
-                    },
-                    detectRetina: true,
-                }}
-            /> */}
-
-            {/* </div> */}
+                        detectRetina: true,
+                    }}
+                />
 
 
-            <div className="projects-container">
                 <h1 ref={projecth1}>Projects</h1>
-                <div ref={projectimg} className="project-container">
-                    <div className='box box1'>
-                        <h2>Project Name: Health Haven Hospital</h2>
+                <div ref={projectimg} className="projects-container">
+
+                    <div className="project-container">
+
+                        <div className='box box3'></div>
+                        <h2>Name: Weather App</h2>
+                        <div className='techbox-container'>
+                            <p>Technology:</p>
+                            <span className='techbox'>JavaScript</span>
+                            <span className='techbox'>Css</span>
+                            <span className='techbox'>Weather Api</span>
+                        </div>
+                        <div>
+                            <Link>See Details</Link>
+                            <a href="https://healthcare-service-react-app.web.app/" target="_blank" rel="noopener noreferrer">Live preview</a>
+                        </div>
                     </div>
-                    <div className='box box2'>
-                        {/* <h2>Project Name: Health Haven Hospital</h2> */}
+
+                    <div className="project-container">
+                        <div className='box box2'></div>
+                        <h2>Name: Skill Squid Eudcation</h2>
+                        <div className='techbox-container'>
+                            <p>Technology:</p>
+                            <span className='techbox'>React</span>
+                            <span className='techbox'>Tailwind</span>
+                            <span className='techbox'>React Router</span>
+
+                        </div>
+                        <div>
+                            <Link>See Details</Link>
+                            <a href="https://react-skillsquid-e-learning.netlify.app/" target="_blank" rel="noopener noreferrer">Live preview</a>
+                        </div>
+
                     </div>
-                    <div className='box box3'>
-                        {/* <h2>Project Name: Health Haven Hospital</h2> */}
-                    </div>
-                    <div className='box box4'>
-                        {/* <h2>Project Name: Health Haven Hospital</h2> */}
+
+                    <div className="project-container">
+
+                        <div className='box box3'></div>
+                        <h2>Name: Weather App</h2>
+                        <div className='techbox-container'>
+                            <p>Technology:</p>
+                            <span className='techbox'>JavaScript</span>
+                            <span className='techbox'>Css</span>
+                            <span className='techbox'>Weather Api</span>
+                        </div>
+                        <div>
+                            <Link>See Details</Link>
+                            <a href="https://weather-info-checker.netlify.app/" target="_blank" rel="noopener noreferrer">Live preview</a>
+                        </div>
                     </div>
                 </div>
-
-            </div>
-        </div>
+            </div></>
     );
 };
 
